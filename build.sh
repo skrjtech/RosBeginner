b31ac14711fabc37332607e2855567503734de39
skrjtech <skrjtech@skrtech.com>
2023-01-19 14:49:17 +0900

    UPDATE

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ Codename=$(lsb_release -cs)
 
 echo $DistributorID $Release $Codename
 
-if [ $Release = "18.04" ] ; then
+if [ $Release = "18.04" ]; then
     . installer/RosVersion1/melodic_version.sh
-elif [ $Release = "20.04" ] ; then
+elif [ $Release = "20.04" ]; then
     . installer/RosVersion1/noetic_version.sh
-elif [ $Release = "22.04" ] ; then
+elif [ $Release = "22.04" ]; then
     installer/RosVersion2/humble_version.sh
 else
     echo '特定のOSバージョンが見つかりません'
