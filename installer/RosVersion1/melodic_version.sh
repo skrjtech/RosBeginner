6bfe9edf405344133eeeccd30a595a4912af10cd
skrjtech <skrjtech@skrtech.com>
2023-01-19 17:08:43 +0900

    update

--- installer/RosVersion1/melodic_version.sh
+++ installer/RosVersion1/melodic_version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+sudo -E apt update 
 sudo apt install -y curl
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
--- installer/RosVersion1/noetic_version.sh
+++ installer/RosVersion1/noetic_version.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
+sudo -E apt update 
 sudo apt install -y curl
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo -E apt install -y  ros-noetic-desktop-full
+sudo -E apt install -y ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
-sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin-tools python3-osrf-pycommon build-essential
+sudo -E apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin-tools python3-osrf-pycommon build-essential
 sudo rm -f /etc/ros/rosdep/sources.list.d/20-default.list
 sudo rosdep init
 rosdep update
