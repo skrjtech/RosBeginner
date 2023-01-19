3d0722e50292e115343f285533ce31b93b415a87
skrjtech <skrjtech@skrtech.com>
2023-01-19 16:08:03 +0900

    update

--- installer/RosVersion1/melodic_version.sh
+++ installer/RosVersion1/melodic_version.sh
@@ -4,7 +4,7 @@ sudo apt install -y curl
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" install -y ros-melodic-desktop-full
+sudo -E apt install -y ros-melodic-desktop-full
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source /opt/ros/melodic/setup.bash
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
